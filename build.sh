76b38db5f194744a5b76602a919a5ffb1cc26a0a
Devi Tanuja Veera <166426256+tanu-119@users.noreply.github.com>
2025-04-07 20:57:10 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Remove any existing model files to prevent LFS issues
-rm -rf model
-mkdir -p model
+#rm -rf model
+#mkdir -p model
 
 # Download the model directly
-wget --no-check-certificate "https://drive.google.com/file/d/1-3KZAIoDLV98_5f9KH84tL07QyQBawxT/view?usp=sharing" -O model/brain_tumor_model.h5
+#wget --no-check-certificate "https://drive.google.com/file/d/1-3KZAIoDLV98_5f9KH84tL07QyQBawxT/view?usp=sharing" -O model/brain_tumor_model.h5
 
 # Start the application
-python app.py
+pip install -r requirements.txt
+python -c "from app import download_model; download_model()"
